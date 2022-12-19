8b3b5cc78879eed52d17a88abf487c10091434a8
Ahsan Arshad <70811519+ahsanarshad777@users.noreply.github.com>
2022-12-19 19:17:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,4 @@ def predict():
             return "Unable to read the file. Please check file extension"
 
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=False, port=8000)
+    app.run()
